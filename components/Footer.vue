2f91d8f21c53b04ad8d170ff33c2159af7f8123d
Gustavo Serbena <gustavo.gs618@gmail.com>
2026-02-06 18:24:05 -0300

    feat: adiciona componente Footer.

--- components/Footer.vue
+++ components/Footer.vue
@@ -46,7 +46,7 @@
                 <svg class="h-5 w-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                 </svg>
-                (41) 3333-3333
+                (41) 3049-2811
               </li>
               <li class="flex items-center">
                 <svg class="h-5 w-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -66,7 +66,7 @@
         </div>
         
         <div class="mt-12 pt-8 border-t border-gray-800 text-center text-gray-400">
-          <p>&copy; 2024 Rentatec. Todos os direitos reservados.</p>
+          <p>&copy; 2026 Rentatec. Todos os direitos reservados.</p>
         </div>
       </div>
     </footer>
