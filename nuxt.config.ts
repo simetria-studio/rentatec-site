fc0af0b0ae54fba559b226bbf2b867a52517472f
Gustavo Serbena <gustavo.gs618@gmail.com>
2026-02-09 11:51:04 -0300

    feat: adiciona integração com Google Tag Manager (GTM) ao `nuxt.config.ts`.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,21 +16,37 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W4KPND7');`
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W4KPND7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          tagPosition: 'bodyOpen'
+        }
+      ],
       title: 'Integrações - Sua Empresa',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { 
-          hid: 'description', 
-          name: 'description', 
-          content: 'Soluções de integrações personalizadas para seu negócio' 
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Soluções de integrações personalizadas para seu negócio'
         }
       ],
       link: [
-        { 
-          rel: 'icon', 
-          type: 'image/x-icon', 
-          href: '/favicon.ico' 
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
         },
         {
           rel: 'stylesheet',
